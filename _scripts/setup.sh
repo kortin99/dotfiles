ce1aee9b03330a87f15a487777115802b2c2ea23
kortin.zhou <kortin1999@outlook.com>
2024-11-06 20:19:40 +0800

    feat: 更新 setup 脚本

--- _scripts/setup.sh
+++ _scripts/setup.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
+# ln -s ./raycast ~/.config/raycast
 
-# ln -s ~/.config/raycast ./raycast
-
-# LINKS=(
-#     "raycast            ~/.config/raycast"
-#     "nvim               ~/.config/nvim"
-#     "yabai              ~/.config/yabai"
-#     "yabai              ~/.config/yabai"
-#     "wezterm            ~/.config/wezterm"
-#     "iterm2             ~/.config/iterm2"
-#     "sketchybar         ~/.config/sketchybar"
-#     "tmux               ~/.config/tmux"
-#     "lazygit            ~/.config/lazygit"
-#     "zsh/.zshrc         ~/.zshrc    "
-#     "git/.gitconfig     ~/.gitconfig"
-#     "hammerspoon        ~/.hammerspoon"
-# )
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 source "$SCRIPT_DIR/../config.sh"
 
@@ -28,28 +13,22 @@ create_symlink() {
 
     # 替换 ~ 为 $HOME
     dst=${dst/#\~/$HOME}
-    echo "Soft link $src to $dst ..."
+    # echo -e "\nSymlink $src to $dst ..."
 
     if [ -e "$dst" ]; then
-        echo "检测到已存在的 $dst 配置文件。"
-        read -p "您想要覆盖现有的配置文件吗？(y/N): " OVERRIDE
+        echo -e "\nDestination $dst already exists."
+        read -p "Do you want to overwrite it? (y/N): " OVERRIDE
 
         if [[ ! $OVERRIDE =~ ^[Yy]$ ]]; then
-            # echo "取消操作。"
+            echo -e "Skipped.\n"
             return
         else
             # 如果用户选择覆盖，则删除旧的链接或目录
-            if [ -L "$dst" ]; then
-                # rm "$dst"
-                echo "[]rm"
-            elif [ -d "$dst" ] || [ -f "$dst" ]; then
-                # rm -rf "$dst"
-                echo "[]rm -rf"
-            fi
+            rm -rf "$dst"
         fi
     fi
 
-    # ln -s "$src" "$dst"
+    ln -s "$src" "$dst"
     # echo "[] 建立连接"
 
     # if [ $? -eq 0 ]; then
